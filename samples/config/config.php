cef5abe2d3e9a8befa1581cb3cb12fc3c110f9cc
Ingo von Kügelgen <ingo.kuegelgen@rocket-internet.de>
2016-05-12 17:33:27 +0200

    getBrands call done

--- samples/config/config.php
+++ samples/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-const SC_API_URL = 'https://rocket:rock4me@sellercenter-api-lazada-th.sellercenter.net/';
-const SC_API_USER = 'maintenance@sellercenter.net';
-const SC_API_KEY = '0ba59f9bd1991191889a88608c5ca84166ed96c6';
+const SC_API_URL = 'http://api.sellercenter/';
+const SC_API_USER = 'admin@sellercenter.net';
+const SC_API_KEY = 'ec1ed2a89183f3ae98dcd8788addc5b5e1a78533';
--- test/RocketLabs/SellerCenterSdk/Endpoint/ProductTest.php
+++ test/RocketLabs/SellerCenterSdk/Endpoint/ProductTest.php
@@ -5,13 +5,16 @@ namespace RocketLabs\SellerCenterSdk\Endpoint;
 use RocketLabs\SellerCenterSdk\Endpoint\Product\Request\Builder\Image;
 use RocketLabs\SellerCenterSdk\Endpoint\Product\Request\GetBrands;
 
+/**
+ * Class ProductTest
+ */
 class ProductTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetBrands()
     {
         $this->assertInstanceOf(GetBrands::class, Endpoints::product()->getBrands());
     }
-    
+
     public function testImage()
     {
         $this->assertInstanceOf(Image::class, Endpoints::product()->image('someSKU'));
